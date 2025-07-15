23c0ca7928b9b1f75ae640fe2775a292bbee514e
Vaosariaka <Andriamanantsoa.vaosariaka@gmail.com>
2025-07-15 15:27:35 +0300

    exemplaire

--- src/main/java/com/example/biblio/repository/ExemplaireRepository.java
+++ src/main/java/com/example/biblio/repository/ExemplaireRepository.java
@@ -3,6 +3,8 @@ package com.example.biblio.repository;
 import com.example.biblio.model.Exemplaire;
 import com.example.biblio.model.Livre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,16 @@ import java.util.Optional;
 @Repository
 public interface ExemplaireRepository extends JpaRepository<Exemplaire, Long> {
     List<Exemplaire> findByLivreAndDisponibleTrue(Livre livre);
+
     Optional<Exemplaire> findByNumExemplaire(String numExemplaire);
+
+    @Query("SELECT COUNT(e) FROM Exemplaire e WHERE e.livre.id = :livreId AND e.disponible = true")
+    Integer countAvailableExemplaires(@Param("livreId") Long livreId);
+
+    @Query("SELECT e FROM Exemplaire e WHERE e.livre.id = :livreId AND e.disponible = true")
+    List<Exemplaire> findByLivreIdAndDisponibleTrue(@Param("livreId") Long livreId);
+
+    List<Exemplaire> findByLivreId(Long livreId);
+
+
 }
